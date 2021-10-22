7c7a54f70895e575a30716c5d86b3a2b7d515a1b
Sharath <Sharus9587@gmail.com>
2021-10-22 21:46:22 +0530

    Added: html and css for footer component

---
+++ src/app/Components/footer/footer.component.spec.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { FooterComponent } from './footer.component';
+
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FooterComponent ],
+      imports:[FormsModule, HttpClientModule, RouterTestingModule,ReactiveFormsModule,MatSnackBarModule]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
